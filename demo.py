0422b0e43f4eb507f4790b907cfaf738041a95de
youssef <youssef.el-kattoufi@universite-paris-saclay.fr>
2024-06-13 20:56:01 +0200

    Code raspberry presque fonctionnelle

--- demo.py
+++ demo.py
@@ -16,8 +16,11 @@ from PIL import Image, ImageDraw, ImageFont
 import keyboard  # Module pour gérer les entrées clavier
 
 # Ouvrir le lien dans un navigateur web
-webbrowser.open("https://www.snapchat.com/add/naslachiente/dX6SMH4gRGOdtCKqVvUksAAAgZWJpaXBqcW9wAY_5bZPtAY_5bY0rAAAAAA")
-# Attendre 15 secondes pour que la page se charge
+#webbrowser.open("https://www.snapchat.com/add/naslachiente/dX6SMH4gRGOdtCKqVvUksAAAgZWJpaXBqcW9wAY_5bZPtAY_5bY0rAAAAAA")
+
+webbrowser.open("file:///C:/Users/elkat/Documents/Python/ReconnaissanceCaractereYounes/carte-bancaire-ing-direct.index.webp")
+
+# Attendre 5 secondes pour que la page se charge
 time.sleep(5)
 fichier = 'img.png'
 print('finish init')
@@ -27,8 +30,12 @@ print('finish init')
 reader = easyocr.Reader(['fr'])
 
 def process_image():
+    x =  516
+    y = 223
+    width = 451
+    height = 734
     # Prendre une capture d'écran
-    screen = pyautogui.screenshot()
+    screen = pyautogui.screenshot(region=(x, y, width, height))
     screen.save(fichier)
 
     # Lire le texte à partir de l'image
@@ -38,7 +45,7 @@ def process_image():
     image_pil = Image.open(fichier)
     draw = ImageDraw.Draw(image_pil)
     font = ImageFont.load_default()  # Utiliser la police par défaut
-    #caca
+    
     # Parcourir les résultats et dessiner les rectangles et le texte
     for (haut, droite, bas, gauche), mot, p in result:
         #Affiche les coordoone du mot trouver
@@ -58,7 +65,7 @@ def process_image():
         draw.text((haut[0] + 6, bas[1] - hauteur_texte - 5), mot, fill=(255, 255, 255, 255), font=font)
 
     # Afficher l'image résultante
-    image_pil.show()
+    #image_pil.show()
 
 # Boucle infinie pour traiter les images
 while True:
@@ -72,3 +79,158 @@ while True:
     #    break  # Sortir de la boucle si la touche 'q' est pressée
     process_image()
     
+
+
+# Importer les modules nécessaires
+import time
+import webbrowser
+import easyocr
+from PIL import Image, ImageDraw, ImageFont
+import keyboard
+from pyvirtualdisplay import Display  # Importer Display depuis pyvirtualdisplay
+import os
+
+# Créer une instance de Display
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+# Définir la variable d'environnement DISPLAY
+os.environ['DISPLAY'] = display.display
+
+# Ouvrir le lien dans un navigateur web
+webbrowser.open("file:///C:/Users/elkat/Documents/Python/ReconnaissanceCaractereYounes/carte-bancaire-ing-direct.index.webp")
+
+# Attendre 5 secondes pour que la page se charge
+time.sleep(5)
+fichier = 'img.png'
+print('finish init')
+
+# Initialiser le lecteur OCR
+reader = easyocr.Reader(['fr'])
+
+def process_image():
+    x =  516
+    y = 223
+    width = 451
+    height = 734
+    # Prendre une capture d'écran
+    screen = pyautogui.screenshot(region=(x, y, width, height))
+    screen.save(fichier)
+
+    # Lire le texte à partir de l'image
+    result = reader.readtext(fichier)
+    
+    # Charger l'image avec PIL
+    image_pil = Image.open(fichier)
+    draw = ImageDraw.Draw(image_pil)
+    font = ImageFont.load_default()  # Utiliser la police par défaut
+    
+    # Parcourir les résultats et dessiner les rectangles et le texte
+    for (haut, droite, bas, gauche), mot, p in result:
+        #Affiche les coordoone du mot trouver
+        #print(haut, bas, mot)
+        #Affiche le mot trouver
+        print(mot)
+        draw.rectangle([haut[0], haut[1], bas[0], bas[1]], outline=(0, 0, 255))
+
+        # Utiliser textbbox pour obtenir la taille du texte
+        bbox = draw.textbbox((0, 0), mot, font=font)
+        largeur_texte = bbox[2] - bbox[0]
+        hauteur_texte = bbox[3] - bbox[1]
+
+        # Dessiner un rectangle rempli pour le texte
+        draw.rectangle([haut[0], bas[1] - hauteur_texte - 10, bas[0], bas[1]], fill=(0, 0, 0))
+        # Dessiner le texte
+        draw.text((haut[0] + 6, bas[1] - hauteur_texte - 5), mot, fill=(255, 255, 255, 255), font=font)
+
+    # Afficher l'image résultante
+    #image_pil.show()
+
+# Boucle infinie pour traiter les images
+while True:
+    #Pour arreter le programme ca ne fonctionne pas
+    #print('APPUIE POUR ARRETER')
+    #time.sleep(2)
+
+    # Vérifier si la touche 'q' est pressée pour quitter la boucle
+    #if keyboard.is_pressed('q'):
+    #    print('je sors de la boucle')
+    #    break  # Sortir de la boucle si la touche 'q' est pressée
+    process_image()
+# Importer les modules nécessaires
+import time
+import webbrowser
+import easyocr
+from PIL import Image, ImageDraw, ImageFont
+import keyboard
+from pyvirtualdisplay import Display  # Importer Display depuis pyvirtualdisplay
+import os
+import pyautogui  # Importer pyautogui
+
+# Créer une instance de Display
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+# Définir la variable d'environnement DISPLAY en tant que chaîne de caractères
+os.environ['DISPLAY'] = str(display.display)
+
+# Ouvrir le lien dans un navigateur web
+webbrowser.open("file:///C:/Users/elkat/Documents/Python/ReconnaissanceCaractereYounes/carte-bancaire-ing-direct.index.webp")
+
+# Attendre 5 secondes pour que la page se charge
+time.sleep(5)
+fichier = 'img.png'
+print('finish init')
+
+# Initialiser le lecteur OCR
+reader = easyocr.Reader(['fr'])
+
+def process_image():
+    x =  516
+    y = 223
+    width = 451
+    height = 734
+    # Prendre une capture d'écran
+    screen = pyautogui.screenshot(region=(x, y, width, height))
+    screen.save(fichier)
+
+    # Lire le texte à partir de l'image
+    result = reader.readtext(fichier)
+    
+    # Charger l'image avec PIL
+    image_pil = Image.open(fichier)
+    draw = ImageDraw.Draw(image_pil)
+    font = ImageFont.load_default()  # Utiliser la police par défaut
+    
+    # Parcourir les résultats et dessiner les rectangles et le texte
+    for (haut, droite, bas, gauche), mot, p in result:
+        #Affiche les coordoone du mot trouver
+        #print(haut, bas, mot)
+        #Affiche le mot trouver
+        print(mot)
+        draw.rectangle([haut[0], haut[1], bas[0], bas[1]], outline=(0, 0, 255))
+
+        # Utiliser textbbox pour obtenir la taille du texte
+        bbox = draw.textbbox((0, 0), mot, font=font)
+        largeur_texte = bbox[2] - bbox[0]
+        hauteur_texte = bbox[3] - bbox[1]
+
+        # Dessiner un rectangle rempli pour le texte
+        draw.rectangle([haut[0], bas[1] - hauteur_texte - 10, bas[0], bas[1]], fill=(0, 0, 0))
+        # Dessiner le texte
+        draw.text((haut[0] + 6, bas[1] - hauteur_texte - 5), mot, fill=(255, 255, 255, 255), font=font)
+
+    # Afficher l'image résultante
+    #image_pil.show()
+
+# Boucle infinie pour traiter les images
+while True:
+    #Pour arreter le programme ca ne fonctionne pas
+    #print('APPUIE POUR ARRETER')
+    #time.sleep(2)
+
+    # Vérifier si la touche 'q' est pressée pour quitter la boucle
+    #if keyboard.is_pressed('q'):
+    #    print('je sors de la boucle')
+    #    break  # Sortir de la boucle si la touche 'q' est pressée
+    process_image()
